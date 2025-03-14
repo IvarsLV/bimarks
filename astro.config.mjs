12ef1be3f840004998231b1eb7df6f6c4ccee540
IvarsLV <ivars.plisko@gmail.com>
2025-03-14 14:56:02 +0200

    Настроен деплой на GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://ivarslv.github.io/bimarks/", // Укажи адрес своего GitHub Pages
+  outDir: "dist", // Папка, в которую Astro билдит файлы
+  base: "/bimarks/", // Обязательный параметр для корректных путей
 });
